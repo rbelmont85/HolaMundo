4ecae0b390a4a50e62864183979df98e01f0633f
CORP\rbelmont <ricardo.belmonte-batlle@sogeti.com>
2017-12-28 10:19:35 +0100

    Incluyendo clase coche

---
+++ src/core/Coche.java
@@ -0,0 +1,33 @@
+package core;
+
+public class Coche {
+	
+	private int ruedas;
+	
+	private int puertas;
+
+	public Coche(int ruedas, int puertas) {
+		super();
+		this.ruedas = ruedas;
+		this.puertas = puertas;
+	}
+
+	public int getRuedas() {
+		return ruedas;
+	}
+
+	public void setRuedas(int ruedas) {
+		this.ruedas = ruedas;
+	}
+
+	public int getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+	
+	
+
+}
