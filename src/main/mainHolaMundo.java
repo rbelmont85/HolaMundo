87fc23b4041bc44d3425228dfd554cf32d7ef186
CORP\rbelmont <ricardo.belmonte-batlle@sogeti.com>
2017-12-28 11:05:19 +0100

    Hola Mundo

--- src/main/mainHolaMundo.java
+++ src/main/mainHolaMundo.java
@@ -8,12 +8,7 @@ public class mainHolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Fibonaker fibo = new Fibonaker();
-		
-		System.out.println(fibo.fibonaze());
-		System.out.println(fibo.fibonaze());
-		System.out.println(fibo.fibonaze());
-		System.out.println(fibo.fibonaze());
+		System.out.println("Hola Mundo");
 		
 		
 
