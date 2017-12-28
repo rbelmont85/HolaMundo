ecbc065c52fd148feee1338e20f9fc9bb0392b69
Teixido Estiarte <veronica.teixido-estiarte@sogeti.com>
2017-12-28 11:01:23 +0100

    four commit vero

--- src/main/mainHolaMundo.java
+++ src/main/mainHolaMundo.java
@@ -8,12 +8,12 @@ public class mainHolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Fibonaker fibo = new Fibonaker();
-		
-		//System.out.println(fibo.fibonaze());
-		//System.out.println(fibo.fibonaze());
-		System.out.println(fibo.fibonaze());
-		System.out.println(fibo.fibonaze());
+
+        List<Integer> l1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> l2 = Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+    
+        System.out.println("Intersection: ");
+        l1.stream().filter(l2::contains).forEach(System.out::println);
 		
 		
 
